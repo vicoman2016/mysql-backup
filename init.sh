eaa2a7a26b785e026c4b67aedf57877cff11775c
罗魔什 <vicoman@163.com>
2024-02-29 16:45:40 +0800

    Update init.sh
    add crond command

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 CRONEXP=$(echo "${CRON:-5 0,12 * * *}" | sed 's/"//g' | sed 's/'"'"/'/g')
-echo -e "${CRONEXP} sh /usr/local/sbin/dumpdb.sh" | crontab -
+echo -e "${CRONEXP} /usr/local/sbin/dumpdb.sh" | crontab -
+crond
 echo "Policy: $CRONEXP"
 echo "Mysql backup job started!"
 tail -f /dev/null
