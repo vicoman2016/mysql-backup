211a1c45088a07a24f0ee28bd81cb4692814fe74
罗魔什 <vicoman@163.com>
2024-09-25 22:10:46 +0800

    更新到2.0 添加对grants的备份

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker build -t wgmac/mysql-backup:1.0.5 -t wgmac/mysql-backup:latest .
+docker compose -f example/docker-compose.yml down
+tag=${1:-latest}
+docker buildx build --platform linux/amd64 -t wgmac/mysql-backup:${tag} -t wgmac/mysql-backup:latest -t harbor.hoyozero.com/library/mysql-backup:${tag}  -t harbor.hoyozero.com/library/mysql-backup:latest .
+
+echo "VERSION=${tag}" > example/.env
+#docker compose -f example/docker-compose.yml  up -d
